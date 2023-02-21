0f26937d4aecc567885416d1df99b36c1e4ee4f6
Martí Paredes <martiparedes19@gmail.com>
2023-02-21 11:51:09 +0100

    Afegit saludar

--- P1/src/Main.java
+++ P1/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         // Això és un comentari.
+        saludar();
         System.out.println("Quèè pasa puta?");
+
+    }
+
+    private static void saludar() {
+        System.out.println("O/");
     }
 }
