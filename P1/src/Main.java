b3fa9d951b737ee48f9b1e17e7cc2dd9fc226004
Atomic_Lettuce <xvivesm@gmail.com>
2023-02-22 18:38:05 +0100

    martí puta pederasta

--- P1/src/Main.java
+++ P1/src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Això és un comentari.
-        saludar();
-        System.out.println("Quèè pasa puta?");
-
-    }
-
-    private static void saludar() {
-        System.out.println("O/");
+        System.out.println("Martí puta pederasta");
     }
 }
