f715d292fc557546968b2b8ad593e184b7689f78
Xavier Vives Marcus <xvivesm@gmail.com>
2023-03-20 17:18:03 +0100

    amarillo amarillo amarillo plátano (no sé què he canviat de sa vista)

--- P2/src/P2/Vista/Vista.java
+++ P2/src/P2/Vista/Vista.java
@@ -1,60 +1,60 @@
 package P2.Vista;
 
 import P2.Main;
-import P2.Model.Tauler;
+import P2.Model.*;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Vista extends JFrame implements WindowListener, ActionListener, MouseListener {
-private JButton play;
-private JButton stop;
-private JButton dimensio;
-private JButton insertpeca;
-private String peca="";
-private TaulesEscacs taulesEscacs;
-private Tauler tauler;
+    private JButton play;
+    private JButton stop;
+    private JButton dimensio;
+    private JButton insertpeca;
+    private String pecaEscollida = "";
+    private TaulesEscacs taulesEscacs;
+    private Tauler tauler;
 
     private Main main;
 
-    public Vista (String nom, Main main, Tauler tauler){
+    public Vista(String nom, Main main, Tauler tauler) {
         super(nom);
         this.main = main;
-        this.tauler=tauler;
+        this.tauler = tauler;
 
-        taulesEscacs = new TaulesEscacs(500,500, tauler);
+        // Panell de dibuixat
+        taulesEscacs = new TaulesEscacs(500, 500, tauler);
         taulesEscacs.addMouseListener(this);
         this.getContentPane().setLayout(new BorderLayout());
-        this.add(taulesEscacs,BorderLayout.CENTER);
+        this.add(taulesEscacs, BorderLayout.CENTER);
 
+        // Zona botonera
         play = new JButton(new ImageIcon("img/play.png"));
-        stop=new JButton(new ImageIcon("img/stop.png"));
-        dimensio=new JButton(new ImageIcon("img/elegirN.png"));
-        insertpeca=new JButton(new ImageIcon("img/elegirpeca.png"));
+        stop = new JButton(new ImageIcon("img/stop.png"));
+        dimensio = new JButton(new ImageIcon("img/elegirN.png"));
+        insertpeca = new JButton(new ImageIcon("img/elegirpeca.png"));
 
-
+        // Zona botonera (identificadors)
         play.setActionCommand("play");
         stop.setActionCommand("stop");
         dimensio.setActionCommand("dimensio");
         insertpeca.setActionCommand("insertpeca");
 
-
+        // Zona botonera (manejador d'events)
         play.addActionListener(this);
         stop.addActionListener(this);
         dimensio.addActionListener(this);
         insertpeca.addActionListener(this);
 
+        // Zona botonera (motius estètics)
         play.setBackground(Color.WHITE);
         stop.setBackground(Color.WHITE);
         dimensio.setBackground(Color.WHITE);
         insertpeca.setBackground(Color.WHITE);
 
-        JPanel botonera=new JPanel();
+        // Zona botonera (afegir-los en pantalla)
+        JPanel botonera = new JPanel();
         botonera.setBackground(Color.YELLOW);
         botonera.add(play);
         botonera.add(stop);
@@ -68,28 +68,29 @@ private Tauler tauler;
         mostrar();
     }
 
-    private void mostrar(){
+    private void mostrar() {
         this.pack();
         this.setVisible(true);
         this.revalidate();
         this.repaint();
     }
 
-    private void llegir_dimensio(){
+
+    // Mètode emprat per llegir input de l'usuari i que aquest estableixi la dimensióm del tauler que vol
+    private void llegir_dimensio() {
         try {
             String str = JOptionPane.showInputDialog("Enter");
-            System.out.println("dimensió: "+Integer.parseInt(str));
-            main.comunicacio("dimensio:"+str);
+            System.out.println("dimensió: " + Integer.parseInt(str));
+            main.comunicacio("dimensio:" + str);
 
-        }catch (NumberFormatException nfe){
+        } catch (NumberFormatException nfe) {
             JOptionPane.showMessageDialog(this, ":( Fica un número sencer major que 0");
         }
 
     }
 
-    private void insertPeca(){
-        String[] opcions ={"Alfil","Cavall","Rei", "Reina", "Super Cavall","Torre"};
-
+    private void insertPeca() {
+        String[] opcions = {"Alfil", "Cavall", "Rei", "Reina", "Super Cavall", "Torre"};
 
         int n = JOptionPane.showOptionDialog(this,
                 "Quina peça vols introduir",
@@ -101,17 +102,17 @@ private Tauler tauler;
                 opcions[1]);
 
         System.out.println("L'usuari ha agafat la opció " + opcions[n]);
-        if(n==-1){
-            peca="";
+        if (n == -1) {
+            pecaEscollida = "";
 
-        }else{
-            peca=opcions[n];
+        } else {
+            pecaEscollida = opcions[n];
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()){
+        switch (e.getActionCommand()) {
             case "play":
                 main.comunicacio("play");
                 break;
@@ -129,18 +130,41 @@ private Tauler tauler;
         }
 
     }
+
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(peca.equals("")){
+        // Si l'usuari no ha sel·leccionat peça, no feim res
+        if (pecaEscollida.equals("")) {
             return;
         }
-
-        int x=e.getX()/(taulesEscacs.getWidth()/tauler.getDim());
-        int y=e.getY()/(taulesEscacs.getHeight()/tauler.getDim());
+        // Obtenir casella on vol ficar peça
+        int x = e.getX() / (taulesEscacs.getWidth() / tauler.getDim());
+        int y = e.getY() / (taulesEscacs.getHeight() / tauler.getDim());
 
         // Enviar ordre de crear peça
-        peca="";
-
+        Peca p=null;
+        switch (pecaEscollida) {
+            case "Alfil":
+                p = new Alfil(tauler.getDim(),x,y);
+                break;
+            case "Cavall":
+                p =new Cavall(x,y);
+                break;
+            case "Rei":
+                p =new Rei(x,y);
+                break;
+            case "Reina":
+                p =new Reina(tauler.getDim(),x,y);
+                break;
+            case "Super Cavall":
+                p =new SuperCavall(x,y);
+                break;
+            case "Torre":
+                p =new Torre(tauler.getDim(),x,y);
+                break;
+        }
+        tauler.afegirPeca(p);
+        pecaEscollida = "";
     }
 
 
@@ -149,6 +173,7 @@ private Tauler tauler;
         dispose();
         //main.comunicacio("Aturar");
     }
+
     @Override
     public void windowOpened(WindowEvent e) {
 
@@ -178,6 +203,7 @@ private Tauler tauler;
     public void windowDeactivated(WindowEvent e) {
 
     }
+
     @Override
     public void mousePressed(MouseEvent e) {
 
