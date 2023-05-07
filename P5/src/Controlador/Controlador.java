49f21b4e00dd2f12de12ec241edf09e78fd902a4
josepruiz14 <jdruiz5e@gmail.com>
2023-05-07 17:49:57 +0200

    p5 template

---
+++ P5/src/Controlador/Controlador.java
@@ -0,0 +1,18 @@
+package Controlador;
+
+
+
+public class Controlador extends Thread{
+
+
+    @Override
+    public void run() {
+
+        System.out.println("Controlador Starts");
+
+
+
+    }
+
+
+}
