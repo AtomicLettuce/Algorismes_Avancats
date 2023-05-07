b3d945e311bc9d1bb60feb7b293e0ba37da06ba2
Xavier Vives Marcus <83074289+AtomicLettuce@users.noreply.github.com>
2023-05-07 18:15:30 +0200

    lector SAX per llegir nodes

--- P4/src/P4/Model/Graf.java
+++ P4/src/P4/Model/Graf.java
@@ -5,6 +5,60 @@ import java.util.ArrayList;
 public class Graf {
     private ArrayList<Node> nodes;
     private ArrayList<Aresta> arestes;
+    private String tipo = "";
+
+    public void ponNodo(String e, int x, int y) {
+        if (!existe(e)) {
+            nodes.add(new Node(e, x, y));
+        } else {
+            System.out.println("Etiqueta ja existent. " +e);
+        }
+    }
+
+    private boolean existe(String e) {
+        boolean res = false;
+        int i = 0;
+        while ((!res) && (i < nodes.size())) {
+            if (e.contentEquals((nodes.get(i)).getEtiqueta())) {
+                res = true;
+            }
+            i++;
+        }
+        return res;
+    }
+
+    public void ponArista(String eti, String etf, double v) {
+        Aresta ar = new Aresta(getNode(etf),v);
+        getNode(eti).ponArista(ar);
+    }
+    public void ponTipo(String t) {
+        tipo = t;
+    }
+    private Node getNode(String e) {
+        boolean res = false;
+        Node n = null;
+        int i = 0;
+        while ((!res) && (i < nodes.size())) {
+            if (e.contentEquals((nodes.get(i)).getEtiqueta())) {
+                res = true;
+                n = nodes.get(i);
+            }
+            i++;
+        }
+        if (n == null) {
+            System.out.println("Node Inexistent");
+        }
+        return n;
+    }
+    public Node getNode(int i) {
+        Node res = null;
+        if (i < nodes.size()) {
+            res = nodes.get(i);
+        } else {
+            System.out.println("Node Inexistent "+i+".");
+        }
+        return res;
+    }
 
 
 }
--- P4/src/P4/Model/Node.java
+++ P4/src/P4/Model/Node.java
@@ -1,4 +1,41 @@
 package P4.Model;
 
+import java.util.ArrayList;
+
 public class Node {
+    private String etiqueta;
+    private ArrayList<Aresta> salientes;
+    private int X;
+    private int Y;
+
+    public Node(String et, int x, int y) {
+        etiqueta = et;
+        X = x;
+        Y = y;
+        salientes = new ArrayList <Aresta> ();
+    }
+
+    protected void ponArista(Aresta a) {
+        salientes.add(a);
+    }
+
+    protected String getEtiqueta() {
+        return etiqueta;
+    }
+
+    protected int getX() {
+        return X;
+    }
+
+    protected int getY() {
+        return Y;
+    }
+
+    protected int getNAristas() {
+        return salientes.size();
+    }
+
+    protected Aresta getArista(int i) {
+        return salientes.get(i);
+    }
 }
