01b85df83e40f8c4afddb3114da30195c254a42d
Xavier Vives Marcus <83074289+AtomicLettuce@users.noreply.github.com>
2023-06-19 18:41:31 +0200

    POLINOMITZACIÓ DE NEWTON

--- P7/src/P7/Main.java
+++ P7/src/P7/Main.java
@@ -17,6 +17,12 @@ public class Main implements InterficieComunicacio {
         new Main().inici();
     }
 
+    //DEIX AIXÒ AQUÍ DE FORMA PROVISIONAL. LLAVORS HO ARREGLARÉ I HO POS ON TOCA
+    double  func(int n){
+        double i =0.000368357066*Math.pow(n,6)-0.0198795751*Math.pow(n,5)+0.43510339*Math.pow(n,4)-4.92310776*Math.pow(n,3)+30.2531138*Math.pow(n,2)-95.3486723*n+119.892874;
+        return i;
+    }
+
     private void inici(){
         System.out.println("UEP!");
 
@@ -32,10 +38,11 @@ public class Main implements InterficieComunicacio {
                 new BigInteger("47081630774621"),
                 new BigInteger("5674018715441897")
         };
-        for(BigInteger numero : numeros ){
+        System.out.println(func(16));
+        /*for(BigInteger numero : numeros ){
             unNumero(numero);
             model.vaciarHashMap();
-        }
+        }*/
 
     }
 
@@ -47,6 +54,7 @@ public class Main implements InterficieComunicacio {
         System.out.println("Voy a factorizar " + numero);
         model.printNumeros();
         System.out.println("He tardado "+(t2-t1)+" nanosec");
+        System.out.println("He tardado "+((double)(t2-t1)/1000000000)+" sec");
     }
 
     @Override
